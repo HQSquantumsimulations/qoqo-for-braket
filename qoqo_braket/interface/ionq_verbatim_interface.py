6a0fb1d5bdd663d794ff4e496b24fbb0df473d55
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2023-07-21 10:51:25 +0200

    Changed copyright notice to be more verbose

--- qoqo_braket/interface/ionq_verbatim_interface.py
+++ qoqo_braket/interface/ionq_verbatim_interface.py
@@ -1,5 +1,14 @@
-# Copyright © 2023 HQS Quantum Simulations GmbH. All Rights Reserved.
-# License details given in distributed LICENSE file.
+# Copyright © 2023 HQS Quantum Simulations GmbH.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
 """Interface for the IonQ AWS braket devices."""
 
 from braket.circuits import Circuit
