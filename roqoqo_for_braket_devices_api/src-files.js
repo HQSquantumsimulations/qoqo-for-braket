4b769ec555022e8068d4af68fd965f8d23088229
kbarkhqs <kbarkhqs@users.noreply.github.com>
2024-03-19 10:53:44 +0000

    deploy: 5d1e343eafbe7db3c4696f9e924deb0ddafcd8c8

--- roqoqo_for_braket_devices_api/src-files.js
+++ roqoqo_for_braket_devices_api/src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"roqoqo_for_braket_devices":["",[["devices",[],["aws_ionq_aria1.rs","aws_ionq_harmony.rs","aws_oqc_lucy.rs","aws_rigetti_aspen_m3.rs"]]],["devices.rs","lib.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["roqoqo_for_braket_devices",["",[["devices",[],["aws_ionq_aria1.rs","aws_ionq_harmony.rs","aws_oqc_lucy.rs","aws_rigetti_aspen_m3.rs"]]],["devices.rs","lib.rs"]]]\
+]'));
 createSrcSidebar();
