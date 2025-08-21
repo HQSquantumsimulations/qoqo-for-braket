b78873fb82cd86d8e50451006ddf3c005a6c85a0
kbarkhqs <kbarkhqs@users.noreply.github.com>
2025-08-21 09:33:52 +0000

    deploy: d844bd60057b1ed1b801bc3353f38c995bd253ca

--- roqoqo_for_braket_devices_api/src-files.js
+++ roqoqo_for_braket_devices_api/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["roqoqo_for_braket_devices",["",[["devices",[],["aws_ionq_aria1.rs","aws_ionq_harmony.rs","aws_oqc_lucy.rs","aws_rigetti_aspen_m3.rs"]]],["devices.rs","lib.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[163]}
+createSrcSidebar('[["roqoqo_for_braket_devices",["",[["devices",[],["aws_ionq_aria1.rs","aws_ionq_harmony.rs","aws_oqc_lucy.rs","aws_rigetti_aspen_m3.rs"]]],["devices.rs","lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[163]}
---
+++ roqoqo_for_braket_devices_api/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["roqoqo_for_braket_devices",[["impl UnsafeUnpin for <a class=\"enum\" href=\"roqoqo_for_braket_devices/devices/enum.AWSDevice.html\" title=\"enum roqoqo_for_braket_devices::devices::AWSDevice\">AWSDevice</a>",1,["roqoqo_for_braket_devices::devices::AWSDevice"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"roqoqo_for_braket_devices/devices/struct.IonQAria1Device.html\" title=\"struct roqoqo_for_braket_devices::devices::IonQAria1Device\">IonQAria1Device</a>",1,["roqoqo_for_braket_devices::devices::aws_ionq_aria1::IonQAria1Device"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"roqoqo_for_braket_devices/devices/struct.IonQHarmonyDevice.html\" title=\"struct roqoqo_for_braket_devices::devices::IonQHarmonyDevice\">IonQHarmonyDevice</a>",1,["roqoqo_for_braket_devices::devices::aws_ionq_harmony::IonQHarmonyDevice"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"roqoqo_for_braket_devices/devices/struct.OQCLucyDevice.html\" title=\"struct roqoqo_for_braket_devices::devices::OQCLucyDevice\">OQCLucyDevice</a>",1,["roqoqo_for_braket_devices::devices::aws_oqc_lucy::OQCLucyDevice"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"roqoqo_for_braket_devices/devices/struct.RigettiAspenM3Device.html\" title=\"struct roqoqo_for_braket_devices::devices::RigettiAspenM3Device\">RigettiAspenM3Device</a>",1,["roqoqo_for_braket_devices::devices::aws_rigetti_aspen_m3::RigettiAspenM3Device"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1406]}
